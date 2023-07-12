614883086009fbfb1a891a1b9b79304bfa7ef7ae
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2023-07-12 13:48:58 -0700

    Fix snapshots deploy workflow (#28)
    * Fix snapshots deploy workflow
    
    * Fix signing

--- snapshots/snapshots-processor/build.gradle.kts
+++ snapshots/snapshots-processor/build.gradle.kts
@@ -113,5 +113,8 @@ publishing {
 }
 
 signing {
+  val signingKey: String? by project
+  val signingPassword: String? by project
+  useInMemoryPgpKeys(signingKey, signingPassword)
   sign(publishing.publications["release"])
 }
