7e567ef9b08ccc9323355385c231dbb5586f2a11
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2023-09-05 21:06:53 -0700

    Add some tests (#62)

---
+++ snapshots/snapshots-processor-test/src/test/resources/PreviewProcessorTest/multipreview_with_three_previews_and_normal_preview_produces_four_snapshots/output/MultiPreviewComposable_-339123963_GenSnapshot.kt
@@ -0,0 +1,39 @@
+package PreviewProcessorTest.multipreview_with_three_previews_and_normal_preview_produces_four_snapshots.input
+
+import PreviewProcessorTest.multipreview_with_three_previews_and_normal_preview_produces_four_snapshots.input.MultiPreviewComposable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.emergetools.snapshots.EmergeSnapshots
+import com.emergetools.snapshots.compose.SnapshotVariantProvider
+import com.emergetools.snapshots.shared.ComposePreviewSnapshotConfig
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+public class `MultiPreviewComposable_-339123963_GenSnapshot` {
+  @get:Rule
+  public val composeRule: ComposeContentTestRule = createComposeRule()
+
+  public val previewConfig: ComposePreviewSnapshotConfig = ComposePreviewSnapshotConfig(originalFqn
+      =
+      "PreviewProcessorTest.multipreview_with_three_previews_and_normal_preview_produces_four_snapshots.input.MultiPreviewComposable",
+      name = "Dark Mode",
+      group = "Dark Mode",
+      uiMode = 32)
+
+  @get:Rule
+  public val snapshotRule: EmergeSnapshots = EmergeSnapshots()
+
+  @Test
+  public fun MultiPreviewComposable_GenSnapshot() {
+    composeRule.setContent {
+      SnapshotVariantProvider(previewConfig) {
+        MultiPreviewComposable()
+      }
+    }
+    snapshotRule.take(composeRule, previewConfig)
+  }
+}
