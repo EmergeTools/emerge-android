07a39410ae4c1dac237ad0d313ab985aec39be3a
Trevor Elkins <trevor@emergetools.com>
2023-09-27 13:40:54 -0400

    Add a locale multi-preview annotation

--- snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/TextRowWithIcon.kt
+++ snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/TextRowWithIcon.kt
@@ -3,6 +3,7 @@ package com.emergetools.snapshots.sample.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.emergetools.snapshots.annotations.IgnoreEmergeSnapshot
 
@@ -25,15 +26,17 @@ fun TextRowWithIcon(
 
 @Preview
 @FontScalePreviews
+@LocalePreviews
 @Composable
 fun TextRowWithIconPreviewFromMain() {
   TextRowWithIcon(
-    titleText = "Title",
-    subtitleText = "Subtitle"
+    titleText = stringResource(com.emergetools.snapshots.sample.R.string.sample_title),
+    subtitleText = stringResource(com.emergetools.snapshots.sample.R.string.sample_subtitle)
   )
 }
 
 @FontScalePreviews
+@LocalePreviews
 @Composable
 fun TextRowWithIconPreviewFromMainJustMultiPreview() {
   TextRowWithIcon(
