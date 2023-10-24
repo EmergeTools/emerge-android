b727209fca7486bc07132e665bec224babbdf98c
Trevor Elkins <trevor@emergetools.com>
2023-10-24 16:14:58 -0400

    Comment

--- snapshots/snapshots/src/main/kotlin/com/emergetools/snapshots/shared/Utils.kt
+++ snapshots/snapshots/src/main/kotlin/com/emergetools/snapshots/shared/Utils.kt
@@ -13,6 +13,10 @@ fun String.normalize(): String {
 const val MAX_KEY_NAME_LENGTH = 128
 
 object EMGLocale {
+  // Android's default `Locale` class doesn't seem to play nicely with regions syntax, like `es-rES`
+  // Instead, we can manually split the locale string ourselves and pass into the appropriate constructor
+  // which seems to work better.
+  // Android Studio has completely separate code for parsing locale codes.
   fun forLanguageCode(code: String): Locale {
     val split = code.split("-")
     if (split.size > 1) {
