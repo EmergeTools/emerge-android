e5d4ebf2fb440efacdaebd0a1bf7cce16b5bac24
Trevor Elkins <trevor@emergetools.com>
2023-10-24 10:39:05 -0400

    Create new context wrapper

--- snapshots/snapshots/src/main/kotlin/com/emergetools/snapshots/compose/SnapshotVariantProvider.kt
+++ snapshots/snapshots/src/main/kotlin/com/emergetools/snapshots/compose/SnapshotVariantProvider.kt
@@ -1,10 +1,12 @@
 package com.emergetools.snapshots.compose
 
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.res.Configuration
-import androidx.activity.compose.LocalActivityResultRegistryOwner
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Resources
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Density
@@ -19,24 +21,46 @@ fun SnapshotVariantProvider(
   val fontScale = config.fontScale ?: 1.0f
   val fontScaleDensity = Density(fontScale = fontScale, density = LocalDensity.current.density)
 
-  val localConfiguration = Configuration(LocalConfiguration.current).apply {
-    config.uiMode?.let { uiMode = it }
-    val locale = config.locale?.let { Locale(it) } ?: Locale.getDefault()
-    setLocale(locale)
-  }
-
-  val localContext = LocalContext.current.createConfigurationContext(localConfiguration)
-
-  val resultRegistryOwner = LocalActivityResultRegistryOwner.current
+  val wrappedContext = SnapshotVariantContextWrapper(
+    LocalContext.current,
+    config.locale?.let { Locale(it) },
+    config.uiMode,
+  )
 
   val providedValues = arrayOf(
-    LocalContext provides localContext,
-    resultRegistryOwner?.let { LocalActivityResultRegistryOwner provides resultRegistryOwner },
+    LocalContext provides wrappedContext,
     config.fontScale?.let { LocalDensity provides fontScaleDensity }
   )
   CompositionLocalProvider(
     values = providedValues.filterNotNull().toTypedArray(),
   ) {
     content()
+  }
+}
+
+class SnapshotVariantContextWrapper(
+  base: Context,
+  private val newLocale: Locale?,
+  private val newUiMode: Int?
+) : ContextWrapper(base) {
+  private var customResources: Resources? = null
+
+  override fun getResources(): Resources {
+    if (customResources == null) {
+      val res = super.getResources()
+      val newConfig = Configuration(res.configuration)
+      newLocale?.let {
+        newConfig.setLocale(it)
+      }
+      newUiMode?.let {
+        newConfig.uiMode = it or (newConfig.uiMode and UI_MODE_NIGHT_MASK.inv())
+      }
+      customResources = super.createConfigurationContext(newConfig).resources
+    }
+    return customResources!!
+  }
+
+  override fun createConfigurationContext(overrideConfiguration: Configuration): Context {
+    return SnapshotVariantContextWrapper(super.createConfigurationContext(overrideConfiguration), newLocale, newUiMode)
   }
 }
