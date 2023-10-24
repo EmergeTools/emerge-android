c1e7481eafc34d104ea38b4f7259942eddc192aa
Trevor Elkins <trevor@emergetools.com>
2023-10-24 16:23:12 -0400

    Cleanup

--- snapshots/snapshots/build.gradle.kts
+++ snapshots/snapshots/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
   implementation(platform(libs.compose.bom))
   implementation(libs.compose.runtime)
   implementation(libs.compose.ui)
-  implementation(libs.activity.compose)
 
   implementation(libs.kotlinx.serialization)
 
