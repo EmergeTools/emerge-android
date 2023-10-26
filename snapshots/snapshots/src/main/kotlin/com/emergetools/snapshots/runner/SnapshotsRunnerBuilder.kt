42d46c9f4f28c9aba78e8c07cda11d6bc369f5c9
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2023-10-26 19:00:26 +0100

    Add additional debug logging to Snapshot SDK (#97)
    * Add additional debug logging to Snapshot SDK
    
    * Lint

--- snapshots/snapshots/src/main/kotlin/com/emergetools/snapshots/runner/SnapshotsRunnerBuilder.kt
+++ snapshots/snapshots/src/main/kotlin/com/emergetools/snapshots/runner/SnapshotsRunnerBuilder.kt
@@ -1,5 +1,6 @@
 package com.emergetools.snapshots.runner
 
+import android.util.Log
 import org.junit.Test
 import org.junit.internal.builders.JUnit4Builder
 import org.junit.runner.Runner
@@ -9,18 +10,25 @@ class SnapshotsRunnerBuilder : JUnit4Builder() {
     // If one method in the class has a @Test method, we can safely assume it's a test class
     // and the SnapshotRunner should handle.
     return if (isTestClass(testClass)) {
+      Log.d(TAG, "Using SnapshotsRunner for class: ${testClass.simpleName}")
       SnapshotsRunner(testClass)
     } else {
       // Fallback to allow AndroidRunnerBuilder to handle the class.
+      Log.d(TAG, "Using default runner for class: ${testClass.simpleName}")
       null
     }
   }
 
   companion object {
+    const val TAG = "SnapshotsRunnerBuilder"
 
     private fun isTestClass(testClass: Class<*>): Boolean {
       // Check if the class has a method annotated with @Test
-      return testClass.methods.any { it.isAnnotationPresent(Test::class.java) }
+      return testClass.methods.any {
+        val annotatedWithTest = it.isAnnotationPresent(Test::class.java)
+        Log.d(TAG, "${testClass.simpleName} method ${it.name} annotated with test: $annotatedWithTest")
+        annotatedWithTest
+      }
     }
   }
 }
