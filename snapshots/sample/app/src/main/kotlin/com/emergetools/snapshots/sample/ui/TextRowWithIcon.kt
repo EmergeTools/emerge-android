a8ffe184773a3664080a97c4f93f18e545f6d372
Trevor Elkins <trevor@emergetools.com>
2023-11-01 15:13:17 -0400

    bump

--- snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/TextRowWithIcon.kt
+++ snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/TextRowWithIcon.kt
@@ -68,7 +68,7 @@ fun TextRowWithIconPreviewFromMainJustStackedMultiPreview() {
 @Composable
 fun TextRowWithIconPreviewFromMainJustSnapshotTestingPreview() {
   TextRowWithIcon(
-    titleText = "Title SnapshotTestingPreview2",
+    titleText = "Title SnapshotTestingPreview22",
     subtitleText = "Subtitle SnapshotTestingPreview"
   )
 }
