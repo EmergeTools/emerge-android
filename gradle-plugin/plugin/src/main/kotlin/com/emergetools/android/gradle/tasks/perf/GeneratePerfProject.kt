c0c6d1df37004b5eb5a23f99b6c2acbc78a7d0a4
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2024-05-20 20:10:41 -0700

    Fix

--- gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/perf/GeneratePerfProject.kt
+++ gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/perf/GeneratePerfProject.kt
@@ -6,9 +6,10 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
-import org.gradle.internal.enterprise.test.FileProperty
 import org.jetbrains.kotlin.gradle.internal.ensureParentDirsCreated
 import java.io.File
 import java.util.zip.ZipInputStream
@@ -32,10 +33,10 @@ abstract class GeneratePerfProject : DefaultTask() {
   @get:Input
   abstract val performanceProjectPath: Property<String>
 
-  @get:Input
+  @get:InputDirectory
   abstract val rootDir: DirectoryProperty
 
-  @get:Input
+  @get:InputFile
   abstract val gradleSettingsFile: RegularFileProperty
 
   @TaskAction
