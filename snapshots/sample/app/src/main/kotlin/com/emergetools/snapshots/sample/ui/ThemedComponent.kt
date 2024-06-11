1f221614fef5dd9d2f45c19c24b1982fade7808a
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2024-06-10 21:20:54 -0700

    Additional large variant

--- snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/ThemedComponent.kt
+++ snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/ThemedComponent.kt
@@ -5,9 +5,6 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.emergetools.snapshots.sample.ui.theme.SnapshotsSampleTheme
 
 @Preview(uiMode = UI_MODE_NIGHT_YES)
@@ -47,6 +43,11 @@ fun ThemedComponent() {
 
 @Preview(name = "tall", heightDp = 1000)
 @Preview(name = "wide", widthDp = 1000)
+@Preview(
+  name = "tall and wide",
+  widthDp = 1000,
+  heightDp = 1000
+)
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Preview(uiMode = UI_MODE_NIGHT_NO)
 @Composable
