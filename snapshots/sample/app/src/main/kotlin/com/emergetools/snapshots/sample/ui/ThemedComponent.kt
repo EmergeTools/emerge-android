1ec96a95e95ea7b4427729724e6c07c30b1b3bba
Trevor Elkins <trevor@emergetools.com>
2024-07-17 18:05:45 -0400

    import

--- snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/ThemedComponent.kt
+++ snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/ThemedComponent.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
