7065beb7b1030d250b46f9b2d0e90fb03cb080ef
Trevor Elkins <trevor@emergetools.com>
2024-07-24 12:38:27 -0400

    Update app store screenshot devices

--- snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/AppStoreScreenshotPreviews.kt
+++ snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/AppStoreScreenshotPreviews.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.emergetools.snapshots.annotations.EmergeAppStoreSnapshot
 
-@Preview(showBackground = true, device = Devices.PIXEL, showSystemUi = true)
-@Preview(showBackground = true, device = Devices.TABLET, showSystemUi = true)
+@Preview(showBackground = true, device = Devices.PIXEL_6_PRO, showSystemUi = true)
+@Preview(showBackground = true, device = Devices.PIXEL_TABLET, showSystemUi = true)
 @EmergeAppStoreSnapshot
 annotation class AppStoreScreenshotPreviews
