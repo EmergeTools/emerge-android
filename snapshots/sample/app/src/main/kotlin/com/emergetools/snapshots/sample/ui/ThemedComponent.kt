f61b3d136eba6f58fbfe264b00ec863eefebf3d7
Trevor Elkins <trevor@emergetools.com>
2024-07-25 17:14:14 -0400

    Show with and without system UI

--- snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/ThemedComponent.kt
+++ snapshots/sample/app/src/main/kotlin/com/emergetools/snapshots/sample/ui/ThemedComponent.kt
@@ -125,39 +125,7 @@ fun SampleShapeWithTextPreview() {
   }
 }
 
-@Preview(showSystemUi = true, device = Devices.NEXUS_7)
-@Preview(showSystemUi = true, device = Devices.NEXUS_7_2013)
-@Preview(showSystemUi = true, device = Devices.NEXUS_5)
-@Preview(showSystemUi = true, device = Devices.NEXUS_6)
-@Preview(showSystemUi = true, device = Devices.NEXUS_9)
-@Preview(showSystemUi = true, device = Devices.NEXUS_10)
-@Preview(showSystemUi = true, device = Devices.NEXUS_5X)
-@Preview(showSystemUi = true, device = Devices.NEXUS_6P)
-@Preview(showSystemUi = true, device = Devices.PIXEL_C)
-@Preview(showSystemUi = true, device = Devices.PIXEL)
-@Preview(showSystemUi = true, device = Devices.PIXEL_XL)
-@Preview(showSystemUi = true, device = Devices.PIXEL_2)
-@Preview(showSystemUi = true, device = Devices.PIXEL_2_XL)
-@Preview(showSystemUi = true, device = Devices.PIXEL_3)
-@Preview(showSystemUi = true, device = Devices.PIXEL_3_XL)
-@Preview(showSystemUi = true, device = Devices.PIXEL_3A)
-@Preview(showSystemUi = true, device = Devices.PIXEL_3A_XL)
-@Preview(showSystemUi = true, device = Devices.PIXEL_4)
-@Preview(showSystemUi = true, device = Devices.PIXEL_4_XL)
-@Preview(showSystemUi = true, device = Devices.PIXEL_4A)
-@Preview(showSystemUi = true, device = Devices.PIXEL_5)
-@Preview(showSystemUi = true, device = Devices.PIXEL_6)
-@Preview(showSystemUi = true, device = Devices.PIXEL_6_PRO)
-@Preview(showSystemUi = true, device = Devices.PIXEL_6A)
-@Preview(showSystemUi = true, device = Devices.PIXEL_7)
-@Preview(showSystemUi = true, device = Devices.PIXEL_7_PRO)
-@Preview(showSystemUi = true, device = Devices.PIXEL_7A)
-@Preview(showSystemUi = true, device = Devices.PIXEL_FOLD)
-@Preview(showSystemUi = true, device = Devices.PIXEL_TABLET)
-@Preview(showSystemUi = true, device = Devices.PHONE)
-@Preview(showSystemUi = true, device = Devices.FOLDABLE)
-@Preview(showSystemUi = true, device = Devices.TABLET)
-@Preview(showSystemUi = true, device = Devices.DESKTOP)
+@DevicePreviews
 @Composable
 fun SampleShapeWithTextPreviewAllDevices() {
   SnapshotsSampleTheme {
