189c76b998c5034cdca648bbecea762dce5a930a
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2024-08-06 16:38:54 -0700

    Make preflight reaper args optional (#222)

--- gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/reaper/PreflightReaper.kt
+++ gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/reaper/PreflightReaper.kt
@@ -8,14 +8,17 @@ import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 
 abstract class PreflightReaper : DefaultTask() {
 
   @get:Input
+  @get:Optional
   abstract val reaperEnabled: Property<Boolean>
 
   @get:Input
+  @get:Optional
   abstract val reaperPublishableApiKey: Property<String>
 
   @get:InputFile
