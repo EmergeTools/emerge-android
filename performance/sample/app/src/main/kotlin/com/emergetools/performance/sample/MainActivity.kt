9f3f2afa46060d026aba2eb675709ab7d116e600
Hector Dearman <hector@emergetools.com>
2024-08-07 14:28:25 +0100

    Add perf example for examining debug.emergetools.is_perf (#220)

--- performance/sample/app/src/main/kotlin/com/emergetools/performance/sample/MainActivity.kt
+++ performance/sample/app/src/main/kotlin/com/emergetools/performance/sample/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.emergetools.performance.sample
 
+import android.annotation.SuppressLint
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import android.os.Trace
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -18,6 +20,16 @@ class MainActivity : ComponentActivity() {
     }
     super.onCreate(savedInstanceState)
 
+    // You should not normally need to detect if you are running under Emergetools
+    // perf test infra however it can be helpful in some situations. In these cases
+    // the recommend method is to check the "debug.emergetools.is_perf" build
+    // property as follows:
+    if (isPerfTest()) {
+      Log.i("PerformanceSample", "Running an Emergetools perf test")
+    } else {
+      Log.i("PerformanceSample", "Not running an Emergetools perftest")
+    }
+
     setContent {
       Column {
         TextRowWithIcon(
@@ -31,5 +43,19 @@ class MainActivity : ComponentActivity() {
     if (VERSION.SDK_INT >= VERSION_CODES.Q) {
       Trace.endAsyncSection("MainActivity.onCreateAsync", 1)
     }
+  }
+
+  @SuppressLint("PrivateApi")
+  @Suppress("SwallowedException", "TooGenericExceptionCaught")
+  fun isPerfTest(): Boolean {
+    var value: String? = null
+    val key = "debug.emergetools.is_perf"
+    try {
+      value = Class.forName("android.os.SystemProperties")
+        .getMethod("get", String::class.java).invoke(null, key) as String
+    } catch (e: Exception) {
+      return false
+    }
+    return value == "1"
   }
 }
