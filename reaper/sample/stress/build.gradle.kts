df69462591eefb21ccea091ef4e3cfdc09a4fb64
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2024-09-19 12:33:33 -0700

    default runner

--- reaper/sample/stress/build.gradle.kts
+++ reaper/sample/stress/build.gradle.kts
@@ -34,7 +34,6 @@ android {
     versionCode = 1
     versionName = "1.0"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
       useSupportLibrary = true
     }
