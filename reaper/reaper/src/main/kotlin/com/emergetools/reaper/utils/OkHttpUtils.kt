282cb9d74232bf734d80e7eceebdd5c81ba45663
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2024-10-24 14:33:49 -0700

    Detekt

--- reaper/reaper/src/main/kotlin/com/emergetools/reaper/utils/OkHttpUtils.kt
+++ reaper/reaper/src/main/kotlin/com/emergetools/reaper/utils/OkHttpUtils.kt
@@ -2,6 +2,7 @@ package com.emergetools.reaper.utils
 
 import java.io.Closeable
 
+@Suppress("TooGenericExceptionCaught")
 internal object OkHttpUtils {
 
   // OkHttp moved this extension function in 5.x of the OkHttp library
