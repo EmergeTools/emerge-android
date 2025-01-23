6deeab2a523c266c72f040ed265e375209333fa3
Nelson Osacky <nelson@emergetools.com>
2025-01-23 17:50:48 +0100

    Bump AGP dependency to 7.4.2 (#427)
    * Bump AGP dependency to 7.4.2
    
    * Fix broken test

--- gradle-plugin/plugin/src/functionalTest/kotlin/com/emergetools/android/gradle/snapshots/MultiProjectSnapshotUploadTest.kt
+++ gradle-plugin/plugin/src/functionalTest/kotlin/com/emergetools/android/gradle/snapshots/MultiProjectSnapshotUploadTest.kt
@@ -2,6 +2,7 @@ package com.emergetools.android.gradle.snapshots
 
 import com.emergetools.android.gradle.EmergePluginTest
 import com.emergetools.android.gradle.base.EmergeGradleRunner
+import com.emergetools.android.gradle.base.EmergeGradleRunner.Companion.LATEST_AGP_7_VERSION
 import com.emergetools.android.gradle.mocks.assertSuccessfulUploadRequests
 import org.junit.jupiter.api.Test
 
@@ -21,7 +22,7 @@ class MultiProjectSnapshotUploadTest : EmergePluginTest() {
   @Test
   fun multiProjectUploadSnapshotBundle_AGP_7_2_0() {
     EmergeGradleRunner.create("multi-project-agp-7.2.0")
-      .withAndroidGradlePluginVersion("7.2.0")
+      .withAndroidGradlePluginVersion(LATEST_AGP_7_VERSION)
       .withGradleVersion("7.5.1")
       .withArguments("emergeUploadSnapshotBundleDebug")
       .withDefaultServer()
