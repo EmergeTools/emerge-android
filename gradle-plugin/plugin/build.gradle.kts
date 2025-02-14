8f8a926ae24daa79eb4ff27b27a51c445d38d763
Nelson Osacky <nelson@emergetools.com>
2025-02-14 15:20:55 +0100

    Shadow KotlinX Serialization to prevent version conflicts

--- gradle-plugin/plugin/build.gradle.kts
+++ gradle-plugin/plugin/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
   alias(libs.plugins.buildconfig)
   alias(libs.plugins.detekt)
   alias(libs.plugins.autonomousapps.testkit)
+  alias(libs.plugins.shadow.plugin)
 
   signing
   `java-gradle-plugin`
@@ -30,6 +31,11 @@ tasks.withType<KotlinCompile>().configureEach {
   compilerOptions {
     jvmTarget.set(JvmTarget.JVM_17)
   }
+}
+
+tasks.named("shadowJar", com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class.java) {
+  relocate("kotlinx.serialization", "emergetools.kotlinx.serialization")
+  archiveClassifier = ""
 }
 
 // This directory will contain one file per version of the Android Gradle Plugin that we wish to test against.
