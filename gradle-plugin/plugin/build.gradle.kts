409fc47e0fe4d3e4001cc6b8d22d761932f6247b
Nelson Osacky <nelson@emergetools.com>
2025-02-21 13:53:37 +0100

    Fix failing tests

--- gradle-plugin/plugin/build.gradle.kts
+++ gradle-plugin/plugin/build.gradle.kts
@@ -147,10 +147,8 @@ dependencies {
   implementation(libs.kotlinx.serialization)
   implementation(libs.okhttp)
 
-  testImplementation(libs.slf4j.api)
   testImplementation(libs.google.truth)
   testImplementation(libs.junit5.jupiter)
-  testRuntimeOnly(gradleApi())
 
   functionalTestImplementation(project(":plugin"))
   functionalTestImplementation(libs.okhttp.mockwebserver)
