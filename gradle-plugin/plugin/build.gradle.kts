c012e1d48c35f89aab6b6fe44e91d62a551be1b2
Nelson Osacky <nelson@emergetools.com>
2025-03-03 18:41:17 +0100

    Use gr8 to relocate classes and create uber jar (#453)

--- gradle-plugin/plugin/build.gradle.kts
+++ gradle-plugin/plugin/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.gradleup.gr8.FilterTransform
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -8,6 +9,7 @@ plugins {
   alias(libs.plugins.buildconfig)
   alias(libs.plugins.detekt)
   alias(libs.plugins.autonomousapps.testkit)
+  alias(libs.plugins.gr8)
 
   signing
   `java-gradle-plugin`
@@ -109,19 +111,33 @@ gradleTestKitSupport {
   withTruthLibrary()
 }
 
+val shadowedDependencies = configurations.create("shadowedDependencies")
+val compileOnlyDependencies: Configuration = configurations.create("compileOnlyDependencies") {
+  attributes {
+    attribute(Usage.USAGE_ATTRIBUTE, project.objects.named<Usage>(Usage.JAVA_API))
+  }
+  attributes {
+    attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, FilterTransform.artifactType)
+  }
+}
+
+compileOnlyDependencies.extendsFrom(configurations.getByName("compileOnly"))
+
 dependencies {
   compileOnly(gradleApi())
   compileOnly(libs.android.gradle.plugin)
 
+  // Ideally we'd like to relocate asm but it causes errors with R8
   implementation(libs.asm)
   implementation(libs.asm.commons)
-  implementation(libs.dexlib2)
+
+  add(shadowedDependencies.name, libs.dexlib2)
   // Needed for the GradleRunner in the functional tests. We've had issues with the version of Guava
   // from one dependency conflicting with that of dexlib2, so we'll use the same version here.
-  implementation(libs.guava)
-  implementation(libs.kotlinx.datetime)
-  implementation(libs.kotlinx.serialization)
-  implementation(libs.okhttp)
+  add(shadowedDependencies.name, libs.guava)
+  add(shadowedDependencies.name, libs.kotlinx.datetime)
+  add(shadowedDependencies.name, libs.kotlinx.serialization)
+  add(shadowedDependencies.name, libs.okhttp)
 
   testImplementation(platform(libs.junit5.bom))
   testImplementation(libs.google.truth)
@@ -135,6 +151,27 @@ dependencies {
   functionalTestRuntimeOnly(libs.junit5.platform.launcher)
 
   detektPlugins(libs.detekt.formatting)
+}
+val shadow = true
+if (shadow) {
+  gr8 {
+    val shadowedJar = create("default") {
+      addProgramJarsFrom(shadowedDependencies)
+      addProgramJarsFrom(tasks.getByName("jar"))
+      proguardFile("rules.pro")
+      registerFilterTransform(listOf(".*/impldep/META-INF/versions/.*"))
+      r8Version("cc8127afa2e852e05b6acd1a29ccc3141f944205")
+    }
+
+    removeGradleApiFromApi()
+
+    replaceOutgoingJar(shadowedJar)
+  }
+  configurations.getByName("compileOnly").extendsFrom(shadowedDependencies)
+  configurations.getByName("testImplementation").extendsFrom(shadowedDependencies)
+  configurations.getByName("functionalTestImplementation").extendsFrom(shadowedDependencies)
+} else {
+  configurations.getByName("implementation").extendsFrom(shadowedDependencies)
 }
 
 gradlePlugin {
---
+++ gradle-plugin/plugin/rules.pro
@@ -0,0 +1,47 @@
+#noinspection ShrinkerUnresolvedReference
+
+# Keep kotlin metadata so that the Kotlin compiler knows about top level functions such as
+#  Line 1: import com.apollographql.apollo.gradle.api.kotlinMultiplatformExtension
+#                                                      ^ Unresolved reference: kotlinMultiplatformExtension
+-keep class kotlin.Metadata { *; }
+-keep class kotlin.Unit { *; }
+
+# Keep the @RequiresOptIn annotation so we get proper warnings in gradle build files
+-keep class kotlin.RequiresOptIn { *; }
+
+# We need to keep type arguments (Signature) for Gradle to be able to instantiate abstract models like `Property`
+# Else it fails with
+# 'Declaration of property alwaysGenerateTypesMatching does not include any type arguments in its property type interface org.gradle.api.provider.SetProperty'
+-keepattributes Signature,InnerClasses,EnclosingMethod
+# Similarly, Gradle needs the @Inject annotations
+-keepattributes RuntimeVisible*Annotation*
+# For debug
+-keepattributes SourceFile,LineNumberTable
+
+-dontobfuscate
+-repackageclasses com.emergetools.android.gradle
+
+# Keep our public api
+-keep class com.emergetools.android.gradle.** { *; }
+-keep interface com.emergetools.android.gradle.** { *; }
+-keep class kotlin.jvm.internal.** { *; }
+-keep interface kotlin.jvm.internal.** { *; }
+-keep class com.emergetools.android.gradle.kotlin.reflect.** { *; }
+-keep interface com.emergetools.android.gradle.kotlin.reflect.** { *; }
+-keep class kotlin.reflect.** { *; }
+-keepclassmembers class com.emergetools.android.gradle.ProductOptions {
+    public <fields>;
+}
+
+# The Gradle API jar and other compileOnly dependencies aren't added to the classpath, ignore the missing symbols
+# I tried adding them but they duplicate a lot of the program classes and trigger errors in R8.
+# A future version could try to remove the intersection between the compileOnly classpath and the runtime one
+-dontwarn org.gradle.**
+-dontwarn org.jetbrains.kotlin.gradle.**
+-dontwarn com.android.build.gradle.**
+-dontwarn com.android.builder.**
+-dontwarn javax.inject.Inject
+-dontwarn kotlin.collections.**
+-dontwarn org.slf4j.**
+-dontwarn com.android.build.**
+-dontwarn org.objectweb.asm.**
