a15741f31944a437c488b873bcc9c4ef67509045
Hector <hector@emergetools.com>
2025-03-05 14:02:06 +0000

    DNM test meta-inf

---
+++ distribution/distribution/src/main/kotlin/com/emergetools/distribution/internal/Foo.java
@@ -0,0 +1,26 @@
+package com.emergetools.distribution.internal;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Foo {
+    public void foo() {
+        InputStream s = this.getClass().getClassLoader().getResourceAsStream("META-INF/version.txt");
+        try {
+            byte[] bytes = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                bytes = s.readAllBytes();
+            }
+            Log.e("!!!!", new String(bytes, StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
