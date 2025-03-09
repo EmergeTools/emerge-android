356c849b12ee24a20e94accf9e1e20a1fbc36d11
Nelson Osacky <nelson@emergetools.com>
2025-03-09 15:24:55 +0100

    Parse annotations referring to other annotations

--- gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/snapshots/transform/FindCustomPreviewClassVisitor.kt
+++ gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/snapshots/transform/FindCustomPreviewClassVisitor.kt
@@ -9,21 +9,11 @@ import org.objectweb.asm.ClassVisitor
  */
 class FindCustomPreviewClassVisitor(
   api: Int,
-  // We need to store the custom annotation classes with their metadata
   private val customPreviewAnnotations: MutableMap<String, CustomPreviewAnnotation>
 ) : ClassVisitor(api) {
 
-  // Boolean to track if the current class is an annotation
-  private var isAnnotationClass = false
-
-  // State for the current annotation being processed
-  private var currentAnnotation = CustomPreviewAnnotation()
-  private var currentClassName = ""
-
-  companion object {
-    // Java class signature for annotation types
-    const val ANNOTATION_TYPE_SIGNATURE = "Ljava/lang/annotation/Retention;"
-  }
+  private val currentAnnotation = CustomPreviewAnnotation()
+  private lateinit var currentClassName : String
 
   override fun visit(
     version: Int,
@@ -34,58 +24,24 @@ class FindCustomPreviewClassVisitor(
     interfaces: Array<out String>?
   ) {
     super.visit(version, access, name, signature, superName, interfaces)
-
-    // Reset state for this class
-    isAnnotationClass = false
-    currentAnnotation = CustomPreviewAnnotation()
     currentClassName = name
-
-    // Check if this is an annotation class (has the annotation interfaces)
-    if (interfaces != null && interfaces.contains("java/lang/annotation/Annotation")) {
-      isAnnotationClass = true
-    }
   }
 
   override fun visitAnnotation(descriptor: String?, visible: Boolean): AnnotationVisitor? {
-    // Only process annotations if this is an annotation class itself
-    if (!isAnnotationClass) {
-      return super.visitAnnotation(descriptor, visible)
+    if (descriptor == EMERGE_APP_STORE_SNAPSHOT) {
+      currentAnnotation.isAppStoreSnapshot = true
+    } else if (customPreviewAnnotations.containsKey(descriptor)) {
+      currentAnnotation.hasPreviewAnnotation = true
+      currentAnnotation.hasPreviewArray = true
+      currentAnnotation.previewConfigs.addAll(customPreviewAnnotations[descriptor]!!.previewConfigs)
     }
 
-    // Check for key annotations we care about
-    when (descriptor) {
-      ANNOTATION_TYPE_SIGNATURE -> {
-        // Confirms this is definitely an annotation
-        isAnnotationClass = true
-      }
-
-      PREVIEW_ANNOTATION_DESC -> {
-        // This annotation has a @Preview directly on it
-        currentAnnotation.hasPreviewAnnotation = true
-
-        // Capture the preview parameters
-        return object : AnnotationVisitor(api) {
-          override fun visit(name: String?, value: Any?) {
-            capturePreviewParameter(name, value)
-            super.visit(name, value)
-          }
-        }
-      }
-
-      EMERGE_APP_STORE_SNAPSHOT -> {
-        // This annotation has @EmergeAppStoreSnapshot
-        currentAnnotation.isAppStoreSnapshot = true
-      }
-    }
-
-    // Special handler for annotations that might contain arrays of @Preview
     return object : AnnotationVisitor(api, super.visitAnnotation(descriptor, visible)) {
       override fun visitArray(name: String?): AnnotationVisitor? {
         if (name == "value") {
           return object : AnnotationVisitor(api) {
             override fun visitAnnotation(name: String?, descriptor: String?): AnnotationVisitor? {
               if (descriptor == PREVIEW_ANNOTATION_DESC) {
-                // This annotation contains an array of @Preview annotations
                 currentAnnotation.hasPreviewArray = true
 
                 // Create a preview configuration to capture parameters
@@ -111,34 +67,13 @@ class FindCustomPreviewClassVisitor(
 
   override fun visitEnd() {
     // If this is an annotation with previews, add it to our map
-    if (isAnnotationClass && (currentAnnotation.hasPreviewAnnotation ||
+    if (currentAnnotation.hasPreviewAnnotation ||
                               currentAnnotation.hasPreviewArray ||
-                              currentAnnotation.isAppStoreSnapshot)) {
+                              currentAnnotation.isAppStoreSnapshot) {
       val descriptor = "L$currentClassName;"
       customPreviewAnnotations[descriptor] = currentAnnotation
     }
     super.visitEnd()
-  }
-
-  /**
-   * Capture preview parameters from an annotation
-   */
-  private fun capturePreviewParameter(name: String?, value: Any?) {
-    when (name) {
-      "name" -> if (value is String) currentAnnotation.name = value
-      "group" -> if (value is String) currentAnnotation.group = value
-      "showBackground" -> if (value is Boolean) currentAnnotation.showBackground = value
-      "backgroundColor" -> if (value is Int) currentAnnotation.backgroundColor = value.toLong()
-      "widthDp" -> if (value is Int) currentAnnotation.widthDp = value
-      "heightDp" -> if (value is Int) currentAnnotation.heightDp = value
-      "showSystemUi" -> if (value is Boolean) currentAnnotation.showSystemUi = value
-      "device" -> if (value is String) currentAnnotation.device = value
-      "fontScale" -> if (value is Float) currentAnnotation.fontScale = value
-      "locale" -> if (value is String) currentAnnotation.locale = value
-      "uiMode" -> if (value is Int) currentAnnotation.uiMode = value
-      "apiLevel" -> if (value is Int) currentAnnotation.apiLevel = value
-      "wallpaper" -> if (value is Int) currentAnnotation.wallpaper = value
-    }
   }
 }
 
