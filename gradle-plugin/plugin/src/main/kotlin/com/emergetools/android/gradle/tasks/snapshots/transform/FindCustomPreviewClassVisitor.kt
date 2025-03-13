1cff14ed9b0a236431aa60a77f5301de92f4ae3c
Nelson Osacky <nelson@emergetools.com>
2025-03-13 17:42:40 +0100

    Parse custom annotations that reference built-in annotations (#523)
    * Parse custom annotations that reference built-in annotations
    
    * Big refactoring

--- gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/snapshots/transform/FindCustomPreviewClassVisitor.kt
+++ gradle-plugin/plugin/src/main/kotlin/com/emergetools/android/gradle/tasks/snapshots/transform/FindCustomPreviewClassVisitor.kt
@@ -2,15 +2,15 @@ package com.emergetools.android.gradle.tasks.snapshots.transform
 
 import org.objectweb.asm.AnnotationVisitor
 import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.Opcodes
 
 /**
  * First pass visitor that identifies custom annotations (like @AppStoreScreenshotPreviews) that are
  * themselves annotated with @Preview or @EmergeAppStoreSnapshot.
  */
 class FindCustomPreviewClassVisitor(
-  api: Int,
   private val customPreviewAnnotations: MutableMap<String, CustomPreviewAnnotation>
-) : ClassVisitor(api) {
+) : ClassVisitor(Opcodes.ASM9) {
 
   private val currentAnnotation = CustomPreviewAnnotation()
   private lateinit var currentClassName: String
@@ -30,10 +30,25 @@ class FindCustomPreviewClassVisitor(
   override fun visitAnnotation(descriptor: String?, visible: Boolean): AnnotationVisitor? {
     if (descriptor == EMERGE_APP_STORE_SNAPSHOT) {
       currentAnnotation.isAppStoreSnapshot = true
+    } else if (descriptor == PREVIEW_ANNOTATION_DESC) {
+      val newConfig = PreviewConfig()
+      currentAnnotation.previewConfigs.add(newConfig)
+      return PreviewAnnotationVisitor(newConfig)
     } else if (customPreviewAnnotations.containsKey(descriptor)) {
-      currentAnnotation.hasPreviewAnnotation = true
-      currentAnnotation.hasPreviewArray = true
-      currentAnnotation.previewConfigs.addAll(customPreviewAnnotations[descriptor]!!.previewConfigs)
+      // If this annotation references another custom annotation we've already found,
+      // copy all its properties to the current annotation
+      val sourceAnnotation = customPreviewAnnotations[descriptor]!!
+
+      if (sourceAnnotation.isAppStoreSnapshot) {
+        currentAnnotation.isAppStoreSnapshot = true
+      }
+
+      // Copy all the preview configs
+      currentAnnotation.previewConfigs.addAll(sourceAnnotation.previewConfigs)
+    }
+    val previewConfigs = previewConfigForAnnotation(descriptor)
+    if (previewConfigs != null) {
+      currentAnnotation.previewConfigs.addAll(previewConfigs)
     }
 
     return object : AnnotationVisitor(api, super.visitAnnotation(descriptor, visible)) {
@@ -42,19 +57,9 @@ class FindCustomPreviewClassVisitor(
           return object : AnnotationVisitor(api) {
             override fun visitAnnotation(name: String?, descriptor: String?): AnnotationVisitor? {
               if (descriptor == PREVIEW_ANNOTATION_DESC) {
-                currentAnnotation.hasPreviewArray = true
-
-                // Create a preview configuration to capture parameters
                 val previewConfig = PreviewConfig()
                 currentAnnotation.previewConfigs.add(previewConfig)
-
-                // Capture the preview parameters
-                return object : AnnotationVisitor(api) {
-                  override fun visit(name: String?, value: Any?) {
-                    previewConfig.captureParameter(name, value)
-                    super.visit(name, value)
-                  }
-                }
+                return PreviewAnnotationVisitor(previewConfig)
               }
               return super.visitAnnotation(name, descriptor)
             }
@@ -66,14 +71,8 @@ class FindCustomPreviewClassVisitor(
   }
 
   override fun visitEnd() {
-    // If this is an annotation with previews, add it to our map
-    if (currentAnnotation.hasPreviewAnnotation ||
-      currentAnnotation.hasPreviewArray ||
-      currentAnnotation.isAppStoreSnapshot
-    ) {
-      val descriptor = "L$currentClassName;"
-      customPreviewAnnotations[descriptor] = currentAnnotation
-    }
+    val descriptor = "L$currentClassName;"
+    customPreviewAnnotations[descriptor] = currentAnnotation
     super.visitEnd()
   }
 }
@@ -82,22 +81,7 @@ class FindCustomPreviewClassVisitor(
  * Data class to store information about a custom annotation like @AppStoreScreenshotPreviews
  */
 data class CustomPreviewAnnotation(
-  var hasPreviewAnnotation: Boolean = false,
-  var hasPreviewArray: Boolean = false,
   var isAppStoreSnapshot: Boolean = false,
-  var name: String? = null,
-  var group: String? = null,
-  var showBackground: Boolean? = null,
-  var backgroundColor: Long? = null,
-  var widthDp: Int? = null,
-  var heightDp: Int? = null,
-  var showSystemUi: Boolean? = null,
-  var device: String? = null,
-  var fontScale: Float? = null,
-  var locale: String? = null,
-  var uiMode: Int? = null,
-  var apiLevel: Int? = null,
-  var wallpaper: Int? = null,
   val previewConfigs: MutableList<PreviewConfig> = mutableListOf()
 )
 
@@ -118,26 +102,4 @@ data class PreviewConfig(
   var uiMode: Int? = null,
   var apiLevel: Int? = null,
   var wallpaper: Int? = null
-) {
-  /**
-   * Capture a preview parameter from an annotation
-   */
-  @Suppress("detekt.CyclomaticComplexMethod")
-  fun captureParameter(name: String?, value: Any?) {
-    when (name) {
-      "name" -> if (value is String) this.name = value
-      "group" -> if (value is String) this.group = value
-      "showBackground" -> if (value is Boolean) this.showBackground = value
-      "backgroundColor" -> if (value is Int) this.backgroundColor = value.toLong()
-      "widthDp" -> if (value is Int) this.widthDp = value
-      "heightDp" -> if (value is Int) this.heightDp = value
-      "showSystemUi" -> if (value is Boolean) this.showSystemUi = value
-      "device" -> if (value is String) this.device = value
-      "fontScale" -> if (value is Float) this.fontScale = value
-      "locale" -> if (value is String) this.locale = value
-      "uiMode" -> if (value is Int) this.uiMode = value
-      "apiLevel" -> if (value is Int) this.apiLevel = value
-      "wallpaper" -> if (value is Int) this.wallpaper = value
-    }
-  }
-}
+)
