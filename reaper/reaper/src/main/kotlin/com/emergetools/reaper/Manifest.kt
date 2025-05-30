b567f2c1bcf1b06d8e345980dd41c129a95704fa
Hector Dearman <hector@emergetools.com>
2025-05-30 11:14:34 +0100

    reaper: Require setting OVERRIDE_BASE_URL (#610)

--- reaper/reaper/src/main/kotlin/com/emergetools/reaper/Manifest.kt
+++ reaper/reaper/src/main/kotlin/com/emergetools/reaper/Manifest.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 
 private const val MANIFEST_TAG_INSTRUMENTED = "com.emergetools.reaper.REAPER_INSTRUMENTED"
 private const val MANIFEST_TAG_PUBLISHABLE_API_KEY = "com.emergetools.reaper.PUBLISHABLE_API_KEY"
+
+// Override is a misnomer now as this is required and there is no base endpoint however
+// don't want to rename this and create more confusion.
 private const val MANIFEST_TAG_OVERRIDE_BASE_URL = "com.emergetools.OVERRIDE_BASE_URL"
 private const val MANIFEST_TAG_DEBUG = "com.emergetools.reaper.DEBUG"
 
@@ -16,7 +19,12 @@ internal fun getApiKey(metadata: Bundle): String {
 }
 
 internal fun getBaseUrl(metadata: Bundle): String {
-  return metadata.getString(MANIFEST_TAG_OVERRIDE_BASE_URL, ReaperConfig.EMERGE_BASE_URL)
+  val baseUrl = metadata.getString(MANIFEST_TAG_OVERRIDE_BASE_URL, "")
+  check(baseUrl != "") {
+    "com.emergetools.OVERRIDE_BASE_URL must be set to a URL. " +
+      "See https://docs.emergetools.com/docs/reaper-setup-android#setting-baseurl"
+  }
+  return baseUrl
 }
 
 internal fun isDebug(metadata: Bundle): Boolean {
